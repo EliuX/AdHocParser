165c0d9e44a90f8b3e032b5d668a1687644482b5
Eliecer Hernandez <eliecerhdz@gmail.com>
2016-03-28 19:06:54 -0500

    Parsing with Parsec

--- src/LocationParser.hs
+++ src/LocationParser.hs
@@ -1,10 +1,15 @@
 module LocationParser(Identifier, Location(..), Point(..), parseSRID, parsePoint, parseLocation) where
 
-import Data.Char(isSpace, isDigit)
-import Data.List
-import AParser
 
--- 32 or 64 bits Integer specifying the Id
+import Control.Applicative (many, some)
+import Control.Monad (void)
+import Data.Char
+import FunctionsAndTypesForParsing
+import Text.Parsec.String (Parser)
+import Text.Parsec.String.Char
+import Text.Parsec.String.Combinator (many1)
+
+-- 32 bits Integer specifying the Id
 type Identifier = Int
 
 -- Specify a location
@@ -19,60 +24,39 @@ data Point = Point { pointX :: Float
 
 -- make a Parser for an Identifier
 parseSRID :: Parser Identifier
-parseSRID = Parser f
-           where f ('S':'R':'I':'D':'=':xs)
-                   | null ns         = Nothing
-                   | otherwise       = case maybeValid ns of
-                                         Just floatValue -> Just (floatValue, skipWhiteSpaces . skipSeparator . skipWhiteSpaces $ rest)
-                                         _               -> Nothing
-                     where (ns, rest) = span isDigit xs
-                 f  _              = Nothing
+parseSRID = do
+    void $ string "SRID="
+    identifier <- many1 digit
+    return (read identifier)
 
 -- Make a Parser for Point
 parsePoint :: Parser Point
-parsePoint  = Parser f
-                     where f ('P':'O':'I':'N':'T':xs) = case unWrap '(' ')' xs of
-                                                         Just x -> runParser (Point <$> parseFloat <*> parseFloat) x
-                                                         _      -> Nothing
-                           f  _                       = Nothing
+parsePoint = do
+      void $ string "POINT("
+      p1 <- takeFloatNum
+      atLeastOneWhiteSpace
+      p2 <- takeFloatNum
+      void $ char ')'
+      return (Point p1 p2)
 
--- Parses a location
 parseLocation :: Parser Location
-parseLocation = Location <$> parseSRID <*> parsePoint
-
-parseFloat:: Parser Float
-parseFloat = Parser f
-  where
-    f xs
-      | null ns   = Nothing
-      | otherwise = Just (read ns, skipWhiteSpaces rest)
-      where (ns, rest) = span (not . isSpace) xs
+parseLocation = Location <$> parseSRID <* expectSeparator <*> parsePoint
 
 
--- Removes whitespaces at the edges
-trim :: String -> String
-trim = f . f
-        where f = reverse . skipWhiteSpaces
+------- Complementary functions -------
 
-skipWhiteSpaces :: String -> String
-skipWhiteSpaces = dropWhile isSpace
+-- Requires at least one whitespace
+atLeastOneWhiteSpace :: Parser ()
+atLeastOneWhiteSpace = void $ some $ oneOf " \n\t"
 
-skipSeparator :: String -> String
-skipSeparator = dropWhile (==';')
+-- Expects a coma delimiting and some whitespaces
+expectSeparator :: Parser ()
+expectSeparator = do
+      void spaces
+      void $ char ';'
+      void spaces
 
-
--- Converts the parsed value a into b
-second :: (c -> b) -> (a,c) -> (a,b)
-second f (a,c) = (a, f c)
-
--- converts to requested element Maybe
-maybeValid :: (Read a) => String -> Maybe a
-maybeValid s = case reads s of
-              [(x, "")] -> Just x
-              _ -> Nothing
-
----- Removes the expected first and last item
-unWrap :: Char -> Char -> String -> Maybe String
-unWrap prefix suffix text = if head text == prefix && last text == suffix
-                                then Just . tail . init $ text
-                                else Nothing
+takeFloatNum :: Parser Float
+takeFloatNum = do
+            val <- many $ satisfy (\a -> isDigit a || a == '-' || a == '.')
+            return (read val)
--- test/LocationParserSpec.hs
+++ test/LocationParserSpec.hs
@@ -2,8 +2,14 @@
 module LocationParserSpec (main, spec) where
 
 import Test.Hspec
+import Data.Either
+import FunctionsAndTypesForParsing
 import LocationParser
-import AParser
+
+-- | @a `returnShouldSatisfy` p@ asserts that the action @a@ returns value that
+--   satisfies predicate @p@.
+returnShouldSatisfy :: (Show a, Eq a) => IO a -> (a -> Bool) -> Expectation
+v `returnShouldSatisfy` p = v >>= (`shouldSatisfy` p)
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -12,42 +18,25 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  let simpleParser = Parser (\x -> Just (x, ""))
-      nullParser   = (Parser (\_ -> Nothing))::(Parser String)
-  describe "Laws of Functor Parser" $ do
-   it "1st functor law : fmap id = id" $ do
-    runParser (fmap id (Parser (\x -> Just (0, x)))) "2" `shouldBe` runParser (id (Parser (\x -> Just (0, x)))) "2"
-    runParser (fmap id nullParser) "2" `shouldBe` runParser (id nullParser) "2"
-   it "2nd functor law : fmap (g . f) = fmap g . fmap f" $ do
-    runParser (fmap ((++ " beautiful") . (++ " world")) simpleParser) "hello" `shouldBe` runParser ((++ " beautiful") <$> (++ " world") <$> (simpleParser)) "hello"
-    runParser (fmap ((++ " beautiful") . (++ " world")) nullParser) "hello" `shouldBe` runParser ((++ " beautiful") <$> (++ " world") <$> nullParser) "hello"
-  describe "Laws of Applicative Parser" $ do
-   it "pure id <*> v = v -- Identity" $ do
-    runParser (pure id <*> simpleParser) "sample" `shouldBe` runParser simpleParser "sample"
-   it "pure f <*> pure x = pure (f x) -- Homomorphism" $ do
-    runParser (pure ("Hola " ++) <*> pure " mundo") " resto" `shouldBe` runParser (pure (("Hola " ++) " mundo")) " resto"
-   it "u <*> pure y = pure ($ y) <*> u -- Interchange" $ do
-    runParser (pure ("Hola" ++) <*> pure " mundo") " resto" `shouldBe` runParser (pure ($ " mundo") <*> pure ("Hola" ++)) " resto"
-   it "pure (.) <*> u <*> v <*> w = u <*> (v <*> w) -- Composition" $ do
-    runParser (pure (.) <*> pure ("Hola " ++) <*> pure ("cruel " ++) <*> pure " mundo") " resto" `shouldBe` runParser (pure ("Hola " ++) <*> (pure ("cruel " ++) <*> pure " mundo")) " resto"
-  describe "parseSRID" $ do
+  describe "parseSRID" $
    it "parses the SRID of the location" $ do
-    runParser parseSRID "SRID=2380" `shouldBe` Just (2380,"")
-    runParser parseSRID "SRID2380"  `shouldBe` Nothing
-  describe "parsePoint" $ do
-    it "parses the Point of the location" $ do
-      runParser parsePoint "POINT 2 3" `shouldBe` Nothing
-      runParser parsePoint "POINT(2 3)" `shouldBe` Just (Point 2 3, "")
-  describe "parseLocation" $ do
-    it "regular case of Location" $ do
-      runParser parseLocation "SRID=2380;POINT(2 3)" `shouldBe` Just (Location { locSRID = 2380, locPoint = Point 2 3}, "")
-    it "The identifier should not accept negative numbers" $ do
-      runParser parseLocation "SRID=-2380;POINT(2 3)" `shouldBe` Nothing
-    it "stric with boundaries" $ do
-      runParser parseLocation "[SRID=2380;POINT(2 3)" `shouldBe` Nothing
-      runParser parseLocation "[SRID=2380;POINT(2 3)+" `shouldBe` Nothing
-      runParser parseLocation "SRID=2380;POINT(2 3)]" `shouldBe` Nothing
-    it "accept negatives coordenate points" $ do
-      runParser parseLocation "SRID=1234;POINT(-2 3.0)" `shouldBe` Just (Location { locSRID = 1234, locPoint = Point (-2) 3}, "")
-    it "accept whitespaces on each segment" $ do
-      runParser parseLocation "SRID=1234 ; POINT(-2 3.0)" `shouldBe` Just (Location { locSRID = 1234, locPoint = Point (-2) 3}, "")
+      regularParse parseSRID "SRID=2380" `shouldBe` Right 2380
+--    regularParse parseSRID "SRID=2380" `shouldBe` Right (2380)
+--    regularParse parseSRID "SRID2380" `returnShouldSatisfy` isLeft
+--  describe "parsePoint" $ do
+--    it "parses the Point of the location" $ do
+--      regularParse parsePoint "POINT 2 3" `returnShouldSatisfy` isLeft
+--      regularParse parsePoint "POINT(2 3)" `shouldBe` Right (Point 2 3)
+--  describe "parseLocation" $ do
+--    it "regular case of Location" $ do
+--      regularParse parseLocation "SRID=2380;POINT(2 3)" `shouldBe` Right (Location { locSRID = 2380, locPoint = Point 2 3})
+--    it "The identifier should not accept negative numbers" $ do
+--      regularParse parseLocation "SRID=-2380;POINT(2 3)" `returnShouldSatisfy` isLeft
+--    it "stric with boundaries" $ do
+--      regularParse parseLocation "[SRID=2380;POINT(2 3)" `returnShouldSatisfy` isLeft
+--      regularParse parseLocation "[SRID=2380;POINT(2 3)+" `returnShouldSatisfy` isLeft
+--      regularParse parseLocation "SRID=2380;POINT(2 3)]" `returnShouldSatisfy` isLeft
+--    it "accept negatives coordenate points" $ do
+--      regularParse parseLocation "SRID=1234;POINT(-2 3.0)" `shouldBe` Right (Location { locSRID = 1234, locPoint = Point (-2) 3})
+--    it "accept whitespaces on each segment" $ do
+--      regularParse parseLocation "SRID=1234 ; POINT(-2 3.0)" `shouldBe` Right (Location { locSRID = 1234, locPoint = Point (-2) 3})
